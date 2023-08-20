31284035034644baf779148e3de02d3fd89aaddb
shweta dand <dandshweta2017@gmail.com>
2023-08-20 15:27:39 +0530

    commit

--- ex/kthLargestFinal.js
+++ ex/kthLargestFinal.js
@@ -50,21 +50,16 @@ function KthElement(arr, k) {
   for (let i = 0; i < k; i++) {
     let max = arr[0];
     let maxIndex = 0;
-
     for (let j = 1; j < arr.length; j++) {
       if (arr[j] > max && !maxValues.includes(arr[j])) {
         max = arr[j];
         maxIndex = j;
       }
     }
-
     maxValues.push(max);
     arr.splice(maxIndex, 1);
   }
-
   //   return maxValues;
-
   return maxValues[maxValues.length - 1];
 }
-
-console.log(KthElement([1, 2, 3, 4, 4, 5, 6], 5));
+console.log(KthElement([21, 22, 23, 24, -23, -45, -56, -67], 2));
