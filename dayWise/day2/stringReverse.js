487b97a5c243d6441628e2134e82b3a26581b0ff
shweta dand <dandshweta2017@gmail.com>
2023-08-25 13:10:38 +0530

    updated

--- dayWise/day2/stringReverse.js
+++ dayWise/day2/stringReverse.js
@@ -1,8 +1,19 @@
 // String reverse with reversing of individual words
 
 function reverseString(str) {
-  let nStr = str.split("");
-  nStr.reverse();
-  return nStr;
+  let nStr = str.split(" ");
+
+  let output = "";
+  for (let i = 0; i < nStr.length; i++) {
+    let word = nStr[i];
+    let wordReverse = "";
+    for (let j = word.length - 1; j >= 0; j--) {
+      wordReverse = wordReverse + word[j];
+    }
+
+    output = output + wordReverse + " ";
+  }
+
+  return output;
 }
-console.log(reverseString("shweta"));
+console.log(reverseString(" hii shweta"));
