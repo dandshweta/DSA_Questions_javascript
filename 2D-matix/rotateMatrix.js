aee6106c2a9949e3db07261de1c65973775fd872
shweta dand <dandshweta2017@gmail.com>
2023-09-07 00:12:09 +0530

    updated

--- 2D-matix/rotateMatrix.js
+++ 2D-matix/rotateMatrix.js
@@ -1,22 +1,22 @@
-function rotateMatrix(arr) {
-  let arr1 = arr[0];
-  let arr2 = arr[1];
-  let newArray1 = [];
-  let newArray2 = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[0]) {
-      newArray1.push(arr1[i]);
-    }
-    if (arr2[1]) {
-      newArray2.push(arr1[i]);
-    }
-  }
-  for (let j = 0; j < arr2.length; j++) {
-    if (arr2[0]) {
-      newArray1.push(arr2[i]);
-    }
-    if (arr2[1]) {
-      newArray2.push(arr2[i]);
-    }
-  }
-}
+// function rotateMatrix(arr) {
+//   let arr1 = arr[0];
+//   let arr2 = arr[1];
+//   let newArray1 = [];
+//   let newArray2 = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[0]) {
+//       newArray1.push(arr1[i]);
+//     }
+//     if (arr2[1]) {
+//       newArray2.push(arr1[i]);
+//     }
+//   }
+//   for (let j = 0; j < arr2.length; j++) {
+//     if (arr2[0]) {
+//       newArray1.push(arr2[i]);
+//     }
+//     if (arr2[1]) {
+//       newArray2.push(arr2[i]);
+//     }
+//   }
+// }
---
+++ 2D-matix/transposeMatrix.js
@@ -0,0 +1,27 @@
+function transposeMatrix(matrix, n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  //   for (let i = 0; i < n; i++) {
+  //     for (let j = 0; j < n / 2; j++) {
+  //       let temp = matrix[i][j];
+  //       matrix[i][j] = matrix[i][n - 1 - j];
+  //       matrix[i][n - 1 - j] = temp;
+  //     }
+  //   }
+  return matrix;
+}
+console.log(
+  transposeMatrix(
+    [
+      [2, 3, 4],
+      [4, 5, 6],
+      [7, 8, 9],
+    ],
+    3
+  )
+);
