16323a6fa2eb311db13b18c3a70468f3882be806
shweta dand <dandshweta2017@gmail.com>
2023-10-15 13:38:17 +0530

    async-await method javascript

--- day11-10-dsa/subArrayWithGivenSum.js
+++ day11-10-dsa/subArrayWithGivenSum.js
@@ -1,58 +1,20 @@
-// function subArrayWithGivenSum(arr, sum) {
-//   //   let count = 0;
-
-//   for (let i = 0; i < arr.length; i++) {
-//     let count = 0;
-
-//     for (j = i + 1; j < arr.length; j++) {
-//       if (count < sum) {
-//         // console.log(sum);
-//         count = count + arr[j];
-//       } else if (count === sum) {
-//         break;
-//       }
-//     }
-
-//     return count === sum;
-//   }
-// }
-// console.log(subArrayWithGivenSum([1, 2, 3, 7, 5], 12));
-// console.log(subArrayWithGivenSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15));
-
-// function subArrayWithGivenSum(arr, sum) {
-//     for (let i = 0; i < arr.length; i++) {
-//       let count = arr[i];
-
-//       for (let j = i + 1; j < arr.length; j++) {
-//         if (count < sum) {
-//           count = count + arr[j];
-//         } else if (count === sum) {
-//           return true;
-//         }
-//       }
-//     }
-
-//     return false;
-//   }
-
-//   console.log(subArrayWithGivenSum([1, 2, 3, 7, 5], 12));
-//   console.log(subArrayWithGivenSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15));
-
 function subArrayWithGivenSum(arr, sum) {
-  for (let i = 1; i <= arr.length; i++) {
-    let currentSum = arr[i];
-    let startIndex = i;
+  for (let i = 0; i < arr.length; i++) {
+    let currentSum = 0;
+    let startIndex = arr[i];
 
-    for (let j = i + 1; j <= arr.length; j++) {
+    for (let j = i; j < arr.length; j++) {
       currentSum = currentSum + arr[j];
+
       if (currentSum === sum) {
-        return [startIndex, j + 1];
+        return [startIndex, arr[j]];
       }
     }
   }
 
-  return null;
+  return `not a subarray like sum is ${sum} `;
 }
 
 console.log(subArrayWithGivenSum([1, 2, 3, 7, 5], 12));
 console.log(subArrayWithGivenSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15));
+console.log(subArrayWithGivenSum([1, 2, 3, 7, 5], 80));
